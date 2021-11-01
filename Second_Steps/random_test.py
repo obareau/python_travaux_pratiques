27251e12163c5044733936e2de13e2dbce605a59
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-01 23:01:39 +0100

    dummy

---
+++ Second_Steps/random_test.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import random
+
+# random.seed(37)
+x = random.random()
+print(x)
+
+
+letters = ("azertyuiopqsdfghjklmwxcvbn")
+print(random.choice(letters))
+
+os = ["Linux", "OSX", "Win10", "Win11", "BSD"]
+print(random.choice(os))
+
+# Rolling dice 
