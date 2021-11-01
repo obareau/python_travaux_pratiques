706dabed5aa3f9b6a810ae82f1529875d251ff3a
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-01 17:57:05 +0100

    ternary op

---
+++ Second_Steps/ternary_operator.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+x = 3
+y = -3
+
+answer_x = "positive" if x > 0 else "negative"
+print(answer_x) # positive
+
+answer_y = "positive" if y > 0 else "negative"
+print(answer_y) # negative
