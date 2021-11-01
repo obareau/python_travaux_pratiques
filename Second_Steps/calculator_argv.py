3a2b19a43c8eb020dd3880d18cbac12f8fd0fa2e
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-01 21:40:54 +0100

    a lot of new stuffs

---
+++ Second_Steps/calculator_argv.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+# ! Multiplication will not work on linux bacause linux use * as a joker character in string
+
+import sys
+
+# print(sys.platform) # Just a test 
+
+system = sys.platform
+
+
+
+if system == "linux":
+    print("Warning ! for multiplication on linux system please use quote around operator (ex : 2 '*'3) ")
+    
+else:
+    print ("You are using non linux system ... everything is fine with multiplication ")
+    
+def main():
+    
+    if len(sys.argv) <4:
+        exit ("Usage: " + sys.argv[0] + " OPERAND OPERATOR OPERAND")
+    
+    a = float(sys.argv [1])
+    b = float(sys.argv [3])
+    op = sys.argv [2]
+        
+    if op == "+":
+        res = a+b
+    elif op == "-":
+        res = a-b
+    elif op == "*":
+        res = a*b
+    elif op == "/":
+        res = a/b
+    else:
+        print("Invalid operator: '{}'".format(op))
+        exit()
+        
+    print(res)
+    
+main()
+        
---
+++ Second_Steps/numbers.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+# ! In Python numbers are stored as decimals but in the source code you can also use octal, hexadecimal or binary notations.
+
+a = 42  # decimal
+h = 0xa # = 10 decimal
+o = 0o11 # 9 octal
+b = 0b11 # 3 binary
+
+print(a) # 42
+print(h) # 10
+print(o) # 9
+print(b) # 3
