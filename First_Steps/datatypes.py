fdde51eec45a8eca18da4f3c031225516a462465
obareau <olivier.bareau@gmail.com>
2021-11-01 12:28:05 +0100

    python base datatypes + Numpy Notes

---
+++ First_Steps/datatypes.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+# Let's play with datatypes
+
+print (type(23))            # int
+print (type(3.14))          # float
+print (type('hello world')) # str
+
+print (type("23"))          # str
+print (type("3.24"))        # str
+
+print (type(None))          # NoneType
+print (type(True))          # bool
+print (type(False))         # bool
+
+print (type([]))            # list
+print (type(()))            # tuple
+print (type({}))            # dict
+
+
+# Values type in Numpy
+# Numpy but also others languages might have them
+
+# int8
+# int32
+# float32
+# float64
+# ... TBC 
