afa7b96f9657747bd3d1e6720ffcab33dda2ea75
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-01 13:44:28 +0100

    print.py

---
+++ Second_Steps/print_py3.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# Let's play with print (python3)function
+
+# print is one of the keywords that changed between v2 and v3
+# it's now a function and requires parentheses
+
+print("hello world", end=" \n ") # \n add new line 
+print("I'm a ", end="guy \n ")
+print ("oliver is a 49 old aged ", end="guy \n ")
