7f0e0c310d4a2f52222e64e1517b6364e977bccf
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-01 17:18:54 +0100

    decimal loss

---
+++ Second_Steps/conv_float_2_int.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+#  ! Conversion often means loss of informations ... 
+
+a = 2.1
+print(type(a)) # class float
+print (a)
+
+b = (int(2.1))
+print(type(b)) # class int32
+print (b) # ! As expected decimal is lost forever :-)
