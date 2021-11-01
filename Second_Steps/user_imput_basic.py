f33af960788acd9cf572bdf5071a21515c39d985
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-01 13:54:43 +0100

    user imput

---
+++ Second_Steps/user_imput_basic.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# Let's play with user imput
+
+def main():
+    print("We have a question!")
+    name = input('Your name: ')
+    print ("hello " + name + ", how are you?")
+    
+# let's call the function!
+
+main()
