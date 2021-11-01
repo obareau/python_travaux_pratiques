ce30c703c75df73ffcc96c5b700ea5741fc6fe8d
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-01 12:43:14 +0100

    some exercices

---
+++ First_Steps/add _numbers_strings.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+# Let's add numbers // strings
+
+
+a = 19
+b = 23
+c = a + b
+print(c) # 42
+
+# WARNING  + is supercharged and cocatenate sttings
+a = "19"
+b = "23"
+c = a + b
+print(c) #1923
---
+++ First_Steps/surface_rectangle.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# Calculate rectagle's surface
+
+width = 23
+height = 17
+
+area = width * height
+print(area) # 391
