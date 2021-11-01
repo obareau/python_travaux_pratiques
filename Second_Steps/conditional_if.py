f7b5aa7b6f9398f4490085d2083fe0facec1e321
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-01 17:29:38 +0100

    dir cleaning

---
+++ Second_Steps/conditional_if.py
@@ -0,0 +1,10 @@
+ # coding: utf-8
+ 
+def main():
+    expected_answer = "42"
+    inp = input("What is the answer? ")
+     
+    if inp == expected_answer :
+        print("The answer to all questions in the universe is 42")
+         
+main()
