548de822aacd85ee5ee3dfb62db9e7ea9981246c
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-02 15:57:31 +0100

    salad improved

--- Second_Steps/rnd_fruits_salad.py
+++ Second_Steps/rnd_fruits_salad.py
@@ -5,6 +5,11 @@
 import random
 
 fruits = [ "Apple", "Pear", "Banana", "Peach", "Orange", "Mango", "Durian", "Papaya", "Watermelon", "Strawberry", "Raspberry", "Kiwi", "Blackcurrant"]
+print("List of ingredients: \n")
+for fruit in  fruits:
+    print (fruit)
+print()
+
 n_ingredient = random.randrange(2, 7) # 2 to 6 cause 7 is not included
 salad = random.sample(fruits, n_ingredient)
 print ("Here is the recipe:",  salad)
