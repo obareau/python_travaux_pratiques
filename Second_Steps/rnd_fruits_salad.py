86d42faac8ff17203c7b4863b10067415b7ce700
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-02 15:41:19 +0100

    more fruits added

--- Second_Steps/rnd_fruits_salad.py
+++ Second_Steps/rnd_fruits_salad.py
@@ -1,10 +1,10 @@
 # coding: utf-8
-
+# The SALADOTRON
 # let's compose a nice fuits salad choose between fruits and a number of ingredient 2 to 6
 
 import random
 
-fruits = [ "Apple", "Pear", "Banana", "Peach", "Orange", "Mango", "Durian", "Papaya", "Watermelon", "Strawberry", "Raspberry"]
+fruits = [ "Apple", "Pear", "Banana", "Peach", "Orange", "Mango", "Durian", "Papaya", "Watermelon", "Strawberry", "Raspberry", "Kiwi", "Blackcurrant"]
 n_ingredient = random.randrange(2, 7) # 2 to 6 cause 7 is not included
 salad = random.sample(fruits, n_ingredient)
 print ("Here is the recipe:",  salad)
