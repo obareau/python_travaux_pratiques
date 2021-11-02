0c1921bae3c66d1ea1aa5c736e0dbfa107c992ea
Olivier Bareau <olivier.bareau@gmail.com>
2021-11-02 15:37:59 +0100

    range modified

--- Second_Steps/rnd_fruits_salad.py
+++ Second_Steps/rnd_fruits_salad.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
-# let's compose a nice fuits salad
+# let's compose a nice fuits salad choose between fruits and a number of ingredient 2 to 6
 
 import random
 
 fruits = [ "Apple", "Pear", "Banana", "Peach", "Orange", "Mango", "Durian", "Papaya", "Watermelon", "Strawberry", "Raspberry"]
-n_ingredient = random.randrange(2, 6)
+n_ingredient = random.randrange(2, 7) # 2 to 6 cause 7 is not included
 salad = random.sample(fruits, n_ingredient)
 print ("Here is the recipe:",  salad)
