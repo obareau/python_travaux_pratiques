28e3a68109839833fbe805b97b0d1dfe66fb506e
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-04 23:15:33 +0200

    test

---
+++ Python_Crash_Courses_V2/O_Numbers.py
@@ -0,0 +1,14 @@
+# Some weird stuff perfectly legit sinnce python 3.6
+universe_age = 14_000_000_000
+print (universe_age)
+# return 14000000000
+
+x, y, z = 14, 'olivier', True
+print (x, y, z)
+# return 14 olivier True
+
+MAX_CONNECTIONS = 5000 
+# A constant is like a variable whose value stays the same throughout the life
+# of a program. Python doesn’t have built-­in constant types, but Python pro-
+# grammers use all capital letters to indicate a variable should be treated as a
+# constant and never be changed:
