5f1ee98ca4ccafff6efd1a855926ef29ceac6cf9
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-04 21:15:21 +0200

    first commit

---
+++ Python_Crash_Courses_V2/name.py
@@ -0,0 +1,19 @@
+# Return name with each words capitalized 
+name = "ada lovelace"
+print(name.title())
+# Ada Lovelace
+
+# Some useful methods
+name2 = "Ada Lovelace"
+print(name2.upper())
+print(name2.lower())
+# ADA LOVELACE
+# ada lovelace
+
+# Using Variables in Strings
+gender = "miss"
+first_name = "ada"
+last_name = "lovelace"
+# f is for f-strings
+full_name = f"{gender} {first_name} {last_name}"
+print(f"\tHello, {full_name.title()}!")
