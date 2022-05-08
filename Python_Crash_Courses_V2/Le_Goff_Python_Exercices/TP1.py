82919c1cb6ace30e73e11d19520ca41ee01b8c5c
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-08 17:24:50 +0200

    first commit

---
+++ Python_Crash_Courses_V2/Le_Goff_Python_Exercices/TP1.py
@@ -0,0 +1,24 @@
+x, y = 4, 3
+print (x)
+print (y)
+
+# Division
+print (x / y) 
+print (y // y) # Division reste entier
+
+
+# Egalité
+print (x > y)
+print (x < y)
+print (y <=x)
+print (y >=x)
+print (x == y) # Egal à 
+print (x != y) # Différent de
+
+# Objet
+print (x is y)
+print (x is not y)
+
+Ma_String = ("un", "deux", "trois")
+
+
