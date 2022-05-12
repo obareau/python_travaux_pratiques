47c498936917a21c53877b579e1b9634eaed9941
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-12 11:09:41 +0200

    first commit

---
+++ Boolean/True_and_False.py
@@ -0,0 +1,15 @@
+# ! True and false are real boolean values
+x = 2
+
+v = x == 2
+print(v)
+if v:
+    print(v, "is true")
+
+    
+v = x == 3
+print(v)
+if v:
+    print(v, "is true")
+else:
+    print(v, "is false")
