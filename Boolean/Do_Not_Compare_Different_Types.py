d49cad1aa19ebe60faa06f434ffb0ed23ac88c87
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-12 23:11:06 +0200

    how to compare length in strings

--- Boolean/Do_Not_Compare_Different_Types.py
+++ Boolean/Do_Not_Compare_Different_Types.py
@@ -1,3 +1,4 @@
+
 x = 12
 y = 3
 print(x > y) # True
@@ -5,7 +6,24 @@ print(x > y) # True
 x = "12"
 y = "3"
 print(x > y) #  ! False / Why it's false ?
+print(x < y) #  ! True
+# x = 12
+# y = "3"
+# print(x > y)
 
-x = 12
-y = "3"
-print(x > y)
+x2 = "45"
+y2 = "321"
+print(x2 > y2) # True 
+
+x2 = "45"
+y2 = "621"
+X2_Length = len(x2)
+Y2_Length = len(y2)
+
+print(X2_Length < Y2_Length) # True 
+
+print( ord('4') ) # 52
+print( ord('5') ) # 53 
+print( ord('6') ) # 54
+print( ord('2') ) # 50
+print( ord('1') ) # 49
