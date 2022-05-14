6d1e21f2d104ce0556baa8984a278c0c85534f84
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-14 12:36:22 +0200

    loops

---
+++ Strings/In_String.py
@@ -0,0 +1,12 @@
+
+txt = "hello world"
+
+if "wo" in txt:
+    print('found wo')
+
+
+if "x" in txt:
+    print("found x")
+
+else:
+    print("NOT found x")
---
+++ Strings/Index_If_In_String.py
@@ -0,0 +1,15 @@
+
+sub = "cat"
+txt = "The black cat climbed the green tree"
+
+
+if sub in txt:
+    loc = txt.index(sub)
+    print(sub + " is at " + str(loc))
+
+
+sub = "dog"
+
+if sub in txt:
+    loc = txt.index(sub)
+    print(sub + " is at " + str(loc))
