624cc4f17f03ba8550477e7f54bdbe7fd154d54b
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-23 11:28:47 +0200

    files2

---
+++ Tp_Files/Filehandle_With_And_Return.py
@@ -0,0 +1,25 @@
+filename = "Tp_Files/examples/files/numbers.txt"
+
+fh = open(filename, 'r')
+
+print(fh)
+# <open file 'numbers.txt', mode 'r' at 0x107084390>
+
+data = fh.read()
+
+# do something with the data
+
+fh.close()
+
+print(fh)
+# <closed file 'numbers.txt', mode 'r' at 0x107084390>
+
+
+
+with open(filename, 'r') as fh:
+
+    print(fh) # <open file 'numbers.txt', mode 'r' at 0x1070840c0>
+    data = fh.read()
+
+print(fh)
+# <closed file 'numbers.txt', mode 'r' at 0x1070840c0>
--- Tp_Files/Open_And_Read_Files.py
+++ Tp_Files/Open_And_Read_Files.py
@@ -1,4 +1,4 @@
-filename = "examples/files/number.txt"
+filename = "Tp_Files/examples/files/numbers.txt" # Relative Path
 
 with open(filename, 'r') as fh:
     for line in fh:
