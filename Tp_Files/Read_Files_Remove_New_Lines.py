b6ceb31ebddbff0f71cb2de33054a3d81de40c43
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-23 12:02:05 +0200

    REMOVE CHAR

--- Tp_Files/Read_Files_Remove_New_Lines.py
+++ Tp_Files/Read_Files_Remove_New_Lines.py
@@ -1,6 +1,9 @@
 filename = "Tp_Files/examples/files/numbers.txt" # Relative Path
 
 with open(filename, 'r') as fh:
+
     for line in fh:
-        print(line, end='')
-        
+
+        line = line.rstrip("*")
+
+        print(line)
