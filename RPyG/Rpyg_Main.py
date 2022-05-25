66087946b2c6b0975be992475e7cd2bc3ca3cabb
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-25 14:19:04 +0200

    test

--- RPyG/Rpyg_Main.py
+++ RPyG/Rpyg_Main.py
@@ -1,3 +1,12 @@
 from rpyg_module import *
 
-print (attack())
+class Pj(object):
+    "Définit le personnage jouable"
+
+    def __init__(self):
+        
+        self.Pv = 0
+        self.Xp =0
+    
+Pj_attack = attack.pt_attack
+
