3ed0a68369076a60d35725b3219492f2749569f5
Olivier BAREAU <olivier.bareau@gmail.com>
2022-05-25 15:26:42 +0200

    test

--- RPyG/Rpyg_Main.py
+++ RPyG/Rpyg_Main.py
@@ -1,12 +1,28 @@
-from rpyg_module import *
+import random
 
 class Pj(object):
     "Définit le personnage jouable"
 
     def __init__(self):
         
-        self.Pv = 0
-        self.Xp =0
+        self.Pv = 25
+        self.Xp =1
     
-Pj_attack = attack.pt_attack
+    def dammage(self):
+        maxrange = 12
+        pt_attack = random.randrange(0, maxrange+1) 
+        xp = 1 # we will work on that later
+        print(pt_attack)
+        return pt_attack
+    
+    def health(self):
+        take_dammage = Pj.Pv - Pj.dammage()
+        
+        print(take_dammage)
+        return take_dammage
 
+    
+PjD1 = Pj()
+titi = PjD1.Pv  - PjD1.dammage()
+
+print(titi)
--- RPyG/rpyg_module.py
+++ RPyG/rpyg_module.py
@@ -5,4 +5,6 @@ def attack():
     maxrange = 12
     pt_attack = random.randrange(0, maxrange+1) 
     xp = 1 # we will work on that later
+    print(pt_attack)
     return pt_attack
+    
